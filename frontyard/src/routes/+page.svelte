3822589ac5dcbe96f61ebd9a8914d49cd4e9f08c
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2026-03-22 17:20:04 +0530

    feat: added some boilerplate

--- frontyard/src/routes/+page.svelte
+++ frontyard/src/routes/+page.svelte
@@ -1,27 +1,49 @@
+<script>
+	let top_courses = $state([
+		{id: 43, name: "Information Security", code: "CSXXXXX", rating: 4.8},
+		{id: 12, name: "Information Insecurity", code: "CSXXXXX", rating: 4.2},
+		{id: 11, name: "Course A", code: "AG01984", rating: 4.1},
+		{id: 21, name: "Course D", code: "EX00451", rating: 3.4},
+	])
+</script>
+
 <input placeholder="Search course name or code" class="main-search" autofocus />
 <div class="top-courses container">
 	<h2 class="title">Top Rated Courses</h2>
 
 	<div class="course-list">
+	{#each top_courses as course, i}
 		<a class="course" href="/course?id=1">
-			<span class="rank">#1</span><span class="name"
-				>Information Security</span
-			>
-			<div class="rating">⭐️⭐️⭐️⭐️⭐️ (4.8)</div>
+			<span class="rank">#{i + 1}</span><span class="name">{course.name} ({course.code})</span>
+			<div class="rating">{'⭐️'.repeat(Math.round(course.rating))} {course.rating}</div>
 		</a>
+	{/each}
+	</div>
+</div>
+
+<div class="top-courses container">
+	<h2 class="title">Top Course Notes</h2>
+
+	<div class="course-list">
+	{#each top_courses as course, i}
 		<a class="course" href="/course?id=1">
-			<span class="rank">#2</span><span class="name"
-				>Information Insecurity</span
-			>
-			<div class="rating">⭐️⭐️⭐️⭐️ (4.2)</div>
+			<span class="rank">#{i + 1}</span><span class="name">{course.name} ({course.code})</span>
+			<div class="rating">{'⭐️'.repeat(Math.round(course.rating))} {course.rating}</div>
 		</a>
+	{/each}
+	</div>
+</div>
+
+
+<div class="top-courses container">
+	<h2 class="title">New Courses</h2>
+
+	<div class="course-list">
+	{#each top_courses as course, i}
 		<a class="course" href="/course?id=1">
-			<span class="rank">#3</span><span class="name">Course A</span>
-			<div class="rating">⭐️⭐️⭐️⭐️ (4.1)</div>
+			<span class="rank">#{i + 1}</span><span class="name">{course.name} ({course.code})</span>
+			<div class="rating">{'⭐️'.repeat(Math.round(course.rating))} {course.rating}</div>
 		</a>
-		<a class="course" href="/course?id=1">
-			<span class="rank">#4</span><span class="name">Course D</span>
-			<div class="rating">⭐️⭐️⭐️ (3.4)</div>
-		</a>
+	{/each}
 	</div>
 </div>
